ca93a7b113fe5898e023f475835ad5d4f0dd076d
Bagus1508 <101942448+Bagus1508@users.noreply.github.com>
2024-08-10 11:54:02 +0700

    Clear Data

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,7 +40,7 @@
                             <div class="mb-4">
                                 <label class="mb-2.5 block font-medium text-black dark:text-white">Username</label>
                                 <div class="relative">
-                                    <input type="email" name="email" id="email" placeholder="Masukan Email Anda"
+                                    <input type="text" name="email" id="email" placeholder="Masukan Email Anda"
                                         class="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary" />
 
                                     <span class="absolute right-4 top-4">
